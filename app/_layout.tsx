01ebf910a31dce30529330c21f6eab9ed555fcbe
Inosthful <inosthful@gmail.com>
2026-01-21 13:54:13 +0100

    Fix: App se lance - Layout root + assets + NativeWind v4 Metro config

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+import React from "react";
+import "../global.css";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
